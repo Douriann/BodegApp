622f920ad1407fc5293506ef4e6b93536e28ecc8
Ailo <alondraleonorsini46@gmail.com>
2025-12-22 18:23:52 -0400

    Logo agregado y Opcion de cambio de Tema

--- src/vistas/VistaPrincipal.py
+++ src/vistas/VistaPrincipal.py
@@ -4,23 +4,17 @@ from vistas.VistaProductos import VistaProductos
 from vistas.VistaTransac import VistaTransac
 from PIL import Image
 
-# Configuración inicial de CustomTkinter
-ctk.set_appearance_mode("System")  # "System", "Dark", "Light"
-ctk.set_default_color_theme("blue") 
 
 class VistaPrincipal(ctk.CTk):
     def __init__(self):
         super().__init__()
 
         # Configuración de la ventana principal
-        #Titulo de la ventana
         self.title("Sistema de Gestión de Inventario")
 
-        #Configurar tamaño fijo y centrar
         anchoVist = 1000
         altoVist = 600
         
-        # Forzamos la actualización para obtener datos reales del monitor
         self.update_idletasks()
         
         anchoPant = self.winfo_screenwidth()
@@ -29,56 +23,63 @@ class VistaPrincipal(ctk.CTk):
         x = (anchoPant // 2) - (anchoVist // 2)
         y = (altoPant // 2) - (altoVist // 2)
 
-        # Aplicamos la posición
         self.geometry(f"{anchoVist}x{altoVist}+{x}+{y}")
         
-        # Configuración del Grid Layout (1 fila, 2 columnas)
-        # Columna 0 = Sidebar (pequeña), Columna 1 = Contenido (grande)
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
+        # --- CREACIÓN DEL SIDEBAR ---
+        self.sidebar_frame = ctk.CTkFrame(self, width=240, fg_color="#9B3D95")
+        self.sidebar_frame.grid(row=0, column=0, sticky="nse")
         
-        # --- 1. CREACIÓN DEL SIDEBAR ---
-        self.sidebar_frame = ctk.CTkFrame(self, width=200, fg_color="#9B3D95")
-        self.sidebar_frame.grid(row=0, column=0, sticky="nse" )
-        self.sidebar_frame.grid_rowconfigure(4, weight=1) # Espacio al final
+        # El weight=1 en la fila 4 empuja todo lo que sigue hacia abajo
+        self.sidebar_frame.grid_rowconfigure(4, weight=1) 
 
-        self.logo_label = ctk.CTkLabel(self.sidebar_frame, text="BodegApp", font=ctk.CTkFont(size=35, weight="bold"))
-        self.logo_label.grid(row=0, column=0, pady=(30, 40))
+        # --- SECCIÓN DEL LOGO (Imagen + Texto) ---
+        # Definir la imagen
+        logo_img = ctk.CTkImage(Image.open("recursos/logo-bodegApp-blanco.png"), size=(170, 70))
 
-        # Botones de navegación
-        # Ahora que 'recursos' está al lado de 'main.py', esta ruta funcionará:
+        # Contenedor de la IMAGEN (Fila 0)
+        self.logo_container = ctk.CTkLabel(
+            self.sidebar_frame, 
+            text="", 
+            image=logo_img
+        )
+        self.logo_container.grid(row=0, column=0, pady=(20, 25), padx=10)
+
+        # Iconos
         icono_dash = ctk.CTkImage(Image.open("recursos/icons-panel-de-control.png"), size=(26, 26))
         icono_prod = ctk.CTkImage(Image.open("recursos/icons-productos.png"), size=(26, 26))
         icono_tran = ctk.CTkImage(Image.open("recursos/icons-transaccion.png"), size=(26, 26))
 
+        # Botones de navegación
         self.btn_dash = ctk.CTkButton(
             self.sidebar_frame, 
-            text="Dashboard",
+            text="Dashboard", 
             image=icono_dash,
-            width=200,   # Ancho en píxeles
-            height=50,   # Alto en píxeles
+            width=230, 
+            height=50, 
             corner_radius=0,
-            font=ctk.CTkFont(family="Verdana", size=16, weight="bold"),
-            fg_color="#9B3D95",       # Morado claro
-            hover_color="#7f2376",    # Morado oscuro 
-            anchor="w",      # Alinea el contenido a la izquierda
+            font=ctk.CTkFont(family="Arial", size=16, weight="bold"),
+            fg_color="#9B3D95", 
+            hover_color="#7f2376", 
+            anchor="w", 
             border_spacing=10,
             command=lambda: self.show_frame("VistaDashboard")
-          )
+        )
         self.btn_dash.grid(row=1, column=0, padx=0, pady=0, sticky="ew")
 
         self.btn_prod = ctk.CTkButton(
             self.sidebar_frame, 
-            text="Productos",
+            text="Productos", 
             image=icono_prod,
-            width=200,   # Ancho en píxeles
-            height=50,   # Alto en píxeles
+            width=230, 
+            height=50, 
             corner_radius=0,
-            font=ctk.CTkFont(family="Verdana", size=16, weight="bold"),
-            fg_color="#9B3D95",       # Morado claro
-            hover_color="#7f2376",    # Morado oscuro 
-            anchor="w",      #Alinea el contenido a la izquierda
+            font=ctk.CTkFont(family="Arial", size=16, weight="bold"),
+            fg_color="#9B3D95", 
+            hover_color="#7f2376", 
+            anchor="w", 
             border_spacing=10,
             command=lambda: self.show_frame("VistaProductos")
         )
@@ -86,47 +87,73 @@ class VistaPrincipal(ctk.CTk):
 
         self.btn_tran = ctk.CTkButton(
             self.sidebar_frame, 
-            text="Transaccion",
+            text="Transaccion", 
             image=icono_tran,
-            width=200,   # Ancho en píxeles
-            height=50,   # Alto en píxeles
+            width=230, 
+            height=50, 
             corner_radius=0,
-            font=ctk.CTkFont(family="Verdana", size=16, weight="bold"),
-            fg_color="#9B3D95",       # Morado claro
-            hover_color="#7f2376",    # Morado oscuro
-            anchor="w",      #Alinea el contenido a la izquierda
+            font=ctk.CTkFont(family="Arial", size=16, weight="bold"),
+            fg_color="#9B3D95", 
+            hover_color="#7f2376", 
+            anchor="w", 
             border_spacing=10,
             command=lambda: self.show_frame("VistaTransac")
         )
         self.btn_tran.grid(row=3, column=0, padx=0, pady=0, sticky="ew")
 
-        # --- 2. ÁREA DE CONTENIDO CENTRAL ---
+        # --- SECCIÓN DE APARIENCIA PERSONALIZADA ---
+
+        icono_tema= ctk.CTkImage(Image.open("recursos/icons-paleta-de-color.png"), size=(20, 20))
+
+        self.appearance_mode_label = ctk.CTkLabel(
+            self.sidebar_frame, 
+            text="  Tema", 
+            image=icono_tema,
+            compound="left",
+            font=ctk.CTkFont(family="Arial", size=14, weight="bold"),
+            anchor="w"
+        )
+        self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0), sticky="w")
+        
+        self.appearance_mode_optionemenu = ctk.CTkOptionMenu(
+            self.sidebar_frame, 
+            values=["Claro", "Oscuro", "Sistema"],
+            command=self.change_appearance_mode_event,
+            height=35,
+            fg_color="#7f2376",          # Color del fondo (morado oscuro)
+            button_color="#7f2376",      # Color del botón/flecha
+            button_hover_color="#5e1a58", # Color al pasar el mouse
+            dropdown_fg_color="#7f2376",  # Color de la lista desplegable
+            dropdown_hover_color="#5e1a58"
+        )
+        self.appearance_mode_optionemenu.grid(row=6, column=0, padx=15, pady=(10, 20), sticky="ew")
+        self.appearance_mode_optionemenu.set("Sistema")
+
+        # --- ÁREA DE CONTENIDO CENTRAL ---
         self.main_container = ctk.CTkFrame(self, corner_radius=0, fg_color="transparent")
         self.main_container.grid(row=0, column=1, sticky="nsew")
-        
-        # Hacemos que el container permita apilar elementos
         self.main_container.grid_rowconfigure(0, weight=1)
         self.main_container.grid_columnconfigure(0, weight=1)
 
-        # --- 3. INICIALIZACIÓN DE VISTAS ---
+        # --- INICIALIZACIÓN DE VISTAS ---
         self.frames = {}
-        
-        # Lista de clases a instanciar
         for F in (VistaDashboard, VistaProductos, VistaTransac):
             page_name = F.__name__
-            # Instanciamos la clase pasando 'self' como controller
             frame = F(parent=self.main_container, controller=self)
             self.frames[page_name] = frame
-            
-            # Colocamos todas en la misma posición (se apilan una sobre otra)
             frame.grid(row=0, column=0, sticky="nsew")
 
-        # Mostrar la primera ventana por defecto
         self.show_frame("VistaDashboard")
 
     def show_frame(self, page_name):
-        """Trae al frente el frame seleccionado"""
         frame = self.frames[page_name]
         frame.tkraise()
 
-    
+    def change_appearance_mode_event(self, seleccion_usuario: str):
+        # Mapeamos la selección en español a la instrucción en inglés
+        if seleccion_usuario == "Claro":
+            ctk.set_appearance_mode("Light")
+        elif seleccion_usuario == "Oscuro":
+            ctk.set_appearance_mode("Dark")
+        elif seleccion_usuario == "Sistema":
+            ctk.set_appearance_mode("System")
